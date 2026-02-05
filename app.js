0b4527c0b81746048a8388b7abd8ac795dd91b58
Matheus-aguiar-hub <marquinhos199613a@gmail.com>
2026-02-04 22:03:00 -0300

    Comentários criado

--- app.js
+++ app.js
@@ -15,25 +15,26 @@ const entradaDeDados = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
-
+//Nome do cliente
 entradaDeDados.question('Digite seu nome: ', function(nome){
     let nomeCliente = nome
-
+    //Nome do produto
     entradaDeDados.question('Digite o nome do produto que você quer: ', function(produto){
         let nomeProduto = produto
-        
+        //Valor da compra
         entradaDeDados.question('Digite o valor da compra: ', function(compra){
             let ValorCompra = compra
-        
+            //Taxa de juros
             entradaDeDados.question('Digite a taxa de juros: ', function(juros){
                 let valorJuros = juros
-
-                entradaDeDados.question('Digite o tempo de pagamento (Mêses ou anos): ', function(pagamento){
+                //Tempo de pagamento
+                entradaDeDados.question('Digite o tempo de pagamento: ', function(pagamento){
                     let tempoPagamento = pagamento
-                    
+                    //Calculo dos juros compostos 
                     let dividindoJuros = (valorJuros/100) 
                     let calcularJuros = (Number(ValorCompra) * (Number(1 + dividindoJuros)) ** Number(tempoPagamento) )
                     let jurosCalculado = (Number(calcularJuros) - Number(ValorCompra))
+                    //Fazendo o comprovante de pagamento 
                     console.log(`*****************Viva Moda***************** 
                     \nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.
                     \nA compra do produto ${nomeProduto}, tem um valor de: ${ValorCompra}.
@@ -41,7 +42,6 @@ entradaDeDados.question('Digite seu nome: ', function(nome){
                     \nO acréscimo realizado ao valor de: ${ValorCompra} será de ${jurosCalculado.toFixed(3)}.
                     \nMuito obrigado por escolher a Viva Moda.
                     \n*******************************************`)
-
                 })
             })
         })
